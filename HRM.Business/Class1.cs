5311ae1797df0bef2706db8f0b6de44f1279a48f
Mihir Joshi <mihir67mj@gmail.com>
2021-03-20 20:03:15 +0530

    Add project files.

---
+++ HRM.Business/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace HRM.Business
+{
+    public class Class1
+    {
+    }
+}
---
+++ HRM.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace HRM.Data
+{
+    public class Class1
+    {
+    }
+}
